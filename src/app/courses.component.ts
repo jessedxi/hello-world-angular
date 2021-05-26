4cc6080726fca214578ada0a442188df00a1aa58
Jesse <jessedaniel@live.ca>
2021-05-26 10:51:45 -0600

    add component selector and template

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,4 +1,10 @@
 
-class CoursesComponent {
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'courses',
+  template: '<h2>Courses</h2>'
+})
+export class CoursesComponent {
 
 }
